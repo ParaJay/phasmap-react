0134d85e7c79a01cd86f2391f3bedcca767196fb
ParaJay <calumjhunter@gmail.com>
2023-03-04 20:15:05 +0000

    now loads all info

--- src/scripts/consts.js
+++ src/scripts/consts.js
@@ -1,4 +1,7 @@
-import { toUnSafeArray } from "./utils";
+import { readInfo, toUnSafeArray } from "./utils";
+
+var expected = 0;
+var processed = 0;
 
 export const maps = [
     "bleasedale", "brownstone_high_school", "camp_woodwind", "edgefield", "grafton", "maple_lodge_campsite", "prison", 
@@ -22,3 +25,39 @@ export const evidenceMap = {
     emf: "EMF 5",
     writing: "Ghost Writing"
 };
+
+export const equipment = [
+    "DOTS Projector", "EMF Reader", "Ghost Writing Book", "Photo Camera", "Spirit Box", "UV Torch", "Video Camera",
+     "Candle", "Crucifix", "Glowstick", "Head Mounted Camera", "Lighter", "Motion Sensor", "Parabolic Microphone", "Salt",
+      "Sanity Pills", "Smudge Sticks", "Sound Sensor", "Strong Torch", "Thermometer", "Tripod", "Weak Torch"];
+
+export const cursedItems = ["Haunted Mirror", "Monkey Paw", "Music Box", "Ouija Board", "Summoning Circle", "Tarot Cards", "Voodoo Doll"];
+export const difficulties = ["Amateur", "Intermediate", "Professional", "Nightmare", "Insanity"];
+
+export const info = {};
+
+export function initInfo() {
+    expected = ghosts.length + equipment.length + cursedItems.length + difficulties.length;
+    processed = 0;
+    clearInfo();
+
+    loadAll(ghosts, "ghosts");
+    loadAll(equipment, "equipment");
+    loadAll(cursedItems, "cursed-items");
+    loadAll(difficulties, "difficulties");
+}
+
+function loadAll(array, dir) { array.forEach(e => loadInfo(e, dir)); }
+
+function loadInfo(el, dir) {
+    readInfo(el.toLowerCase().replaceAll(" ", ""), dir).then(e => {
+        info[el] = e;
+        processed++;
+    });
+}
+
+function clearInfo() { Object.keys(info).forEach(e => delete info[e]); }
+
+export function isLoading() { return expected > processed; }
+
+export function getProgress() { return processed + "/" + expected; }
