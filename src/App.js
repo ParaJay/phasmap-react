01f83f291f30fed294a6ba0897f5dbfcf3544102
ParaJay <calumjhunter@gmail.com>
2023-03-04 20:14:43 +0000

    now waits for consts.js to load

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import Nav from './Nav';
 import Main from './Main';
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
+import { initInfo, isLoading } from './scripts/consts';
 
 class App extends React.Component {
+    state = {loaded: false};
+
+    constructor(props) {
+        super(props);
+
+        this.loaded = false;
+
+        initInfo();
+
+        this.init();
+    }
+
+    init() {
+        if(isLoading() || !this.mounted) {
+            setTimeout(() => {this.init()}, 100);
+        } else {
+            this.loaded = true;
+        }
+
+        if(this.mounted) this.forceUpdate();
+    }
+
+    componentDidMount() { this.mounted = true; }
     
     render() {
+        if(!this.loaded) return;
+
         return (
-            <div className="App">
+            <HashRouter>
                 <Nav/>
                 <Main/>
-            </div>
+            </HashRouter>
         );
     }
 }
