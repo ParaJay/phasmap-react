d18f1f1ddf86733b4d52ef02e9610dae6c9e4c3f
ParaJay <calumjhunter@gmail.com>
2023-03-04 12:11:04 +0000

    optim, uses consts

--- src/Nav.js
+++ src/Nav.js
@@ -1,21 +1,16 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {unsafe_maps} from "./scripts/consts";
 
-const maps = [
-    "Bleasedale", "Brownstone High School", "Camp Woodwind", "Edgefield", "Grafton", "Maple Lodge Campsite", "Prison", 
-    "Ridgeview", "Sunny Meadows", "Sunny Meadows Restricted", "Tanglewood", "Willow"
-]
+const maps = unsafe_maps;
 
-const pages = [
-    "Ghosts", "Index", "Journal", "Cursed Items", "Equipment", "Difficulty"
-]
+const pages = ["Ghosts", "Index", "Journal", "Cursed Items", "Equipment", "Difficulty"]
 
 const names = {"Index" : "Maps"};
 
 class Nav extends React.Component {
     render() {
-        let mc = [];
-        let pc = [];
+        let mc = [], pc = [];
 
         for(let i = 0; i < maps.length; i++) {
             let p = "/phasmap-react/?map=" + maps[i].toLowerCase().replaceAll(" ", "%20") + "#/index";
@@ -27,25 +22,16 @@ class Nav extends React.Component {
             let pg = pages[i];
             let n = names[pg] ? names[pg] : pg;
             let p = "/" + pg.toLowerCase().replace(" ", "");
-            pc.push(<button key={"b" + i}><Link key={n} to={p} onClick={(e) => {
-                // e.preventDefault();
-
-                // window.location = window.location.toString().split("/")[0] + "/#/" + p;
-            }}>{n}</Link></button>);
+            pc.push(<button key={"b" + i}><Link key={n} to={p}>{n}</Link></button>);
         }
 
         return (
             <nav id="nav" className="navbar">
                 <div className="dropdown">
-                    <button className="dropbtn">
-                        View Map
-                    </button>
+                    <button className="dropbtn">View Map</button>
                     
-                    <div className="dropdown-content">
-                        {mc}
-                    </div>
+                    <div className="dropdown-content">{mc}</div>
                 </div>
-
                 {pc}
             </nav>
         )
