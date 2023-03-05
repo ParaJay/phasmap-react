845134d66abba338713a14fffb0c213128ad18af
ParaJay <calumjhunter@gmail.com>
2023-03-05 10:48:46 +0000

    moved main.css to index.js

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,5 @@
-import "./styles/main.css";
 import React from 'react';
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Ghosts from './scripts/ghosts';
 import Maps from './scripts/maps';
 import CursedItem from './scripts/cursed-items';
@@ -26,15 +25,6 @@ const Main = () => {
       <Route exact path='/equipment' element={<Equipment/>}></Route>
       <Route exact path='/journal' element={<Journal/>}></Route>
       <Route exact path='/difficulty' element={<Difficulty/>}></Route>
-
-      {/* <Route path='/#/' element={<Maps/>}></Route>
-      <Route path='/#/index' element={<Maps/>}></Route>
-      <Route path='/#/ghosts' element={<Ghosts/>}></Route>
-      <Route path='/#/curseditems' element={<CursedItem/>}></Route>
-      <Route path='/#/equipment' element={<Equipment/>}></Route>
-      <Route path='/#/journal' element={<Journal/>}></Route>
-      <Route path='/#/difficulty' element={<Difficulty/>}></Route> */}
-      {/* <Route exact path='/signup' component={Signup}></Route> */}
     </Routes>
   );
 }
